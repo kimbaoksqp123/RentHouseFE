d170e75a7e6d1434f1916854cf6560e8b232871e
bao-dimage <bao-nguyen@dimage.co.jp>
2024-02-26 10:09:22 +0700

    fix baseURL backend

--- src/apis/customAxios.js
+++ src/apis/customAxios.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: "https://cwy5doxgyfv4ga6c3l7lhzuha40epngj.lambda-url.ap-southeast-1.on.aws/api",
-  // paramsSerializer: params => queryString.stringify(params),
+    baseURL: "http://localhost:8000/api",
+    // paramsSerializer: params => queryString.stringify(params),
 });
 
 customAxios.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (config) => {
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 customAxios.interceptors.response.use(
-  (response) => {
-    if (response && response.data) {
-      return response.data;
+    (response) => {
+        if (response && response.data) {
+            return response.data;
+        }
+        return response;
+    },
+    (error) => {
+        console.log('>>Error:', error);
+        return Promise.reject(error);
     }
-    return response;
-  },
-  (error) => {
-    console.log('>>Error:', error);
-    return Promise.reject(error);
-  }
 );
 
 export default customAxios;
