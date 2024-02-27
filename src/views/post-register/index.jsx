64742b6c600f07ea5a39d0c3d838582411d74ada
bao-dimage <bao-nguyen@dimage.co.jp>
2024-02-27 16:44:04 +0700

    Fix Form Register Post

--- src/views/post-register/index.jsx
+++ src/views/post-register/index.jsx
@@ -15,7 +15,7 @@ const formItemLayout = {
       span: 5,
     },
     sm: {
-      span: 3,
+      span: 4,
     },
   },
   wrapperCol: {
@@ -123,188 +123,250 @@ export default function PostRegister() {
       console.error("Error sending data to API:", error);
     }
   };
-  
 
   return (
     <div className="main-box">
       <div className="left-box">
-        <div className="post-detail">
-        <Form
-      {...formItemLayout}
-      style={{ width: 900, marginTop: 20 }}
-      form={form}
-      onFinish={onFinish}
-    >
-      <Form.Item name="userID" label="User ID" hidden></Form.Item>
-      <Form.Item name="house_typeID" label="Kiểu nhà trọ" rules={[
-          {
-            required: true,
-            message: "Hãy nhập tên phòng trọ",
-          },
-        ]}>
-        <Select placeholder="Chọn kiểu trọ">
-          <Select.Option value="1">Chung cư Mini</Select.Option>
-          <Select.Option value="2">Homestay</Select.Option>
-          <Select.Option value="3">Nhà nguyên căn</Select.Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        name="name"
-        label="Tên phòng trọ"
-        rules={[
-          {
-            required: true,
-            message: "Hãy nhập tên phòng trọ",
-          },
-        ]}
-      >
-        <Input placeholder="Nhập tên phòng trọ"></Input>
-      </Form.Item>
-      <Form.Item name="address" label="Địa chỉ" rules={[
-          {
-            required: true,
-            message: "Hãy nhập tên phòng trọ",
-          },
-        ]}>
-        <Input placeholder="Nhập địa chỉ"></Input>
-      </Form.Item>
-      <Form.Item
-        name="acreage"
-        label="Diện tích"
-        rules={[
-          {
-            type: "number",
-            message: "Diện tích phải là chữ số",
-          },
-          {
-            required: true,
-            message: "Hãy nhập diện tích!",
-          },
-          {
-            validator: (_, value) => {
-              if (value <= 0) {
-                return Promise.resolve();
-              }
-              return Promise.reject("Diện tích phải lớn hơn 0!");
-            },
-          },
-        ]}
-      >
-        <Input placeholder="Nhập diện tích"></Input>
-      </Form.Item>
-      <Form.Item
-        name="number_of_people"
-        label="Số người ở tối đa"
-        rules={[
-          {
-            type: "number",
-            message: "Số người ở tối đa là chữ số",
-          },
-          {
-            required: true,
-            message: "Hãy nhập Số người ở tối đa!",
-          },
-          {
-            validator: (_, value) => {
-              if (value <= 0) {
-                return Promise.resolve();
-              }
-              return Promise.reject("Số người ở tối đa phải lớn hơn 0!");
-            },
-          },
-        ]}
-      >
-        <Input placeholder="Nhập số người ở tối đa"></Input>
-      </Form.Item>
-      <Form.Item
-        name="cost"
-        label="Giá thuê(VNĐ)"
-        rules={[
-          {
-            type: "number",
-            message: "Giá thuê là chữ số",
-          },
-          {
-            required: true,
-            message: "Hãy nhập Giá thuê!",
-          },
-          {
-            validator: (_, value) => {
-              if (value <= 0) {
-                return Promise.resolve();
-              }
-              return Promise.reject("Giá thuê phải lớn hơn 0!");
-            },
-          },
-        ]}
-      >
-        <Input placeholder="Nhập giá thuê"></Input>
-      </Form.Item>
-      <Form.Item name="description" label="Mô tả" rules={[
-          {
-            required: true,
-            message: "Hãy nhập mô tả",
-          },
-        ]}>
-        <Input placeholder="Nhập mô tả"></Input>
-      </Form.Item>
-      <Form.Item name="image" label="Upload" valuePropName="image">
-        <Upload
-          action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={handlePreview}
-          onChange={handleChange}
-        >
-          {fileList.length >= 1 ? null : uploadButton}
-        </Upload>
-        <Modal
-          open={previewOpen}
-          title={previewTitle}
-          footer={null}
-          onCancel={handleCancel}
-        >
-          <img
-            alt="example"
-            style={{
-              width: "100%",
-            }}
-            src={previewImage}
-          />
-        </Modal>
-      </Form.Item>
-      <Form.Item name="imageAlbum" label="Upload" valuePropName="imageAlbum">
-        <Upload
-          action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-          listType="picture-card"
-          fileList={imageAlbum}
-          onPreview={handlePreview}
-          onChange={handleImageAlbum}
-        >
-          {imageAlbum.length >= 2 ? null : uploadButton}
-        </Upload>
-        <Modal
-          open={previewOpen}
-          title={previewTitle}
-          footer={null}
-          onCancel={handleCancel}
-        >
-          <img alt="example" style={{ width: "100%" }} src={previewImage} />
-        </Modal>
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          span: 12,
-          offset: 10,
-        }}
-      >
-        <Space>
-          <Button type="primary" htmlType="submit" size="large">
-            Submit
-          </Button>
-        </Space>
-      </Form.Item>
-    </Form>
+        <div className="post-register">
+          <Form {...formItemLayout} form={form} onFinish={onFinish}>
+            <Form.Item name="userID" label="User ID" hidden></Form.Item>
+            <Form.Item
+              name="house_typeID"
+              label="Kiểu nhà trọ"
+              rules={[
+                {
+                  required: true,
+                  message: "Hãy nhập tên phòng trọ",
+                },
+              ]}
+            >
+              <Select placeholder="Chọn kiểu trọ">
+                <Select.Option value="0">Phòng trọ</Select.Option>
+                <Select.Option value="1">Nhà nguyên căn</Select.Option>
+                <Select.Option value="2">Chung cư Mini</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              name="name"
+              label="Tên phòng trọ"
+              rules={[
+                {
+                  required: true,
+                  message: "Hãy nhập tên phòng trọ",
+                },
+              ]}
+            >
+              <Input placeholder="Nhập tên phòng trọ"></Input>
+            </Form.Item>
+            <Form.Item
+              name="address"
+              label="Địa chỉ"
+              rules={[
+                {
+                  required: true,
+                  message: "Hãy nhập tên phòng trọ",
+                },
+              ]}
+            >
+              <Input placeholder="Nhập địa chỉ"></Input>
+            </Form.Item>
+            <Form.Item
+              name="land_area"
+              label="Diện tích"
+              rules={[
+                {
+                  type: "number",
+                  message: "Diện tích phải là chữ số",
+                },
+                {
+                  required: true,
+                  message: "Hãy nhập diện tích!",
+                },
+                {
+                  validator: (_, value) => {
+                    if (value <= 0) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("Diện tích phải lớn hơn 0!");
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Nhập diện tích"></Input>
+            </Form.Item>
+            <Form.Item
+              name="number_of_people"
+              label="Số người ở tối đa"
+              rules={[
+                {
+                  type: "number",
+                  message: "Số người ở tối đa là chữ số",
+                },
+                {
+                  required: true,
+                  message: "Hãy nhập Số người ở tối đa!",
+                },
+                {
+                  validator: (_, value) => {
+                    if (value <= 0) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("Số người ở tối đa phải lớn hơn 0!");
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Nhập số người ở tối đa"></Input>
+            </Form.Item>
+            <Form.Item
+              name="price"
+              label="Giá thuê(VNĐ)"
+              rules={[
+                {
+                  type: "number",
+                  message: "Giá thuê là chữ số",
+                },
+                {
+                  required: true,
+                  message: "Hãy nhập Giá thuê!",
+                },
+                {
+                  validator: (_, value) => {
+                    if (value <= 0) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("Giá thuê phải lớn hơn 0!");
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Nhập giá thuê"></Input>
+            </Form.Item>
+            <Form.Item
+              name="description"
+              label="Mô tả"
+              rules={[
+                {
+                  required: true,
+                  message: "Hãy nhập mô tả",
+                },
+              ]}
+            >
+              <Input placeholder="Nhập mô tả"></Input>
+            </Form.Item>
+            <Form.Item
+              name="bedroom_num"
+              label="Số phòng ngủ"
+              rules={[
+                {
+                  type: "number",
+                  message: "số phòng ngủ là chữ số",
+                },
+                {
+                  required: true,
+                  message: "Hãy nhập số phòng ngủ!",
+                },
+                {
+                  validator: (_, value) => {
+                    if (value <= 0) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("số phòng ngủ phải lớn hơn 0!");
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Nhập số phòng ngủ"></Input>
+            </Form.Item>
+            <Form.Item
+              name="bathroom_num"
+              label="Số phòng tắm"
+              rules={[
+                {
+                  type: "number",
+                  message: "Số phòng tắm là chữ số",
+                },
+                {
+                  required: true,
+                  message: "Hãy nhập Số phòng tắm!",
+                },
+                {
+                  validator: (_, value) => {
+                    if (value <= 0) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject("Số phòng tắm phải lớn hơn 0!");
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Nhập Số phòng tắm"></Input>
+            </Form.Item>
+            <Form.Item name="image" label="Upload" valuePropName="image">
+              <Upload
+                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                listType="picture-card"
+                fileList={fileList}
+                onPreview={handlePreview}
+                onChange={handleChange}
+              >
+                {fileList.length >= 1 ? null : uploadButton}
+              </Upload>
+              <Modal
+                open={previewOpen}
+                title={previewTitle}
+                footer={null}
+                onCancel={handleCancel}
+              >
+                <img
+                  alt="example"
+                  style={{
+                    width: "100%",
+                  }}
+                  src={previewImage}
+                />
+              </Modal>
+            </Form.Item>
+            <Form.Item
+              name="imageAlbum"
+              label="Upload"
+              valuePropName="imageAlbum"
+            >
+              <Upload
+                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                listType="picture-card"
+                fileList={imageAlbum}
+                onPreview={handlePreview}
+                onChange={handleImageAlbum}
+              >
+                {imageAlbum.length >= 2 ? null : uploadButton}
+              </Upload>
+              <Modal
+                open={previewOpen}
+                title={previewTitle}
+                footer={null}
+                onCancel={handleCancel}
+              >
+                <img
+                  alt="example"
+                  style={{ width: "100%" }}
+                  src={previewImage}
+                />
+              </Modal>
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{
+                span: 12,
+                offset: 10,
+              }}
+            >
+              <Space>
+                <Button type="primary" htmlType="submit" size="large">
+                  Submit
+                </Button>
+              </Space>
+            </Form.Item>
+          </Form>
         </div>{" "}
       </div>
 
