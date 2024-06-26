bd95a14660ec211d6dda26b4931f3ac10309956d
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-27 00:52:47 +0700

    fix loading

--- src/components/RightFilter.jsx
+++ src/components/RightFilter.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
-import { Input, InputNumber } from "antd";
+import React, { useState, useContext, useEffect } from "react";
+import { Input, InputNumber, Spin } from "antd";
 import { PostContext } from "../routes";
 import { filterPostsWithinScope } from "../apis/matrixAPI";
+
 export default function AdvanceFilter({ marginTop }) {
   const { listPost, setListPost, filterCondition } = useContext(PostContext);
   const [address, setAddress] = useState("");
   const [scope, setScope] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+
   const handleFilterClick = async () => {
-    // Gọi hàm filterPostsWithinScope ở đây
-    const filteredPosts = await filterPostsWithinScope(
-      address,
-      listPost,
-      scope
-    );
-    setListPost(filteredPosts);
+    setLoading(true);
+    try {
+      const filteredPosts = await filterPostsWithinScope(address, listPost, scope);
+      setListPost(filteredPosts);
+    } catch (error) {
+      console.error("Error filtering posts:", error);
+    } finally {
+      setLoading(false);
+    }
   };
- 
+
+  useEffect(() => {
+    setLoading(false);
+  }, [listPost, filterCondition]);
+
   return (
     <div>
-      <div
-        className="border-main rounded shadow-sm"
-        style={{ marginTop: marginTop }}
-      >
+      <div className="border-main rounded shadow-sm" style={{ marginTop: marginTop }}>
         <div className="mt-1 ms-3 mb-2 fw-700">Lọc nâng cao</div>
         <div className="ms-3 me-3 mb-3">
           <label htmlFor="address" className="form-label">
@@ -49,10 +54,11 @@ export default function AdvanceFilter({ marginTop }) {
         <div className="ms-3 me-3 mb-3">
           <button
             type="button"
-            className="btn btn-primary"
+            className={`w-20 h-8 bg-green-400 border border-gray-300 rounded-2xl flex items-center justify-center text-white text-base font-semibold shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50`}
             onClick={handleFilterClick}
+            disabled={loading}
           >
-            Lọc
+            {loading ? <Spin size="small" /> : "Lọc"}
           </button>
         </div>
       </div>
