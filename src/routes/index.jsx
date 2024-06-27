3f02d991bda51e4bd0ad6c5e68582fca13ab2ee9
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-27 14:05:55 +0700

    spin

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "../views/layout";
-import Home from "../views/home";
-import PostDetail from "../views/post-detail";
-import PostRegister from "../views/post-register";
-import RequestViewHouse from "../views/request";
-import Manager from "../views/manager";
-import { createContext, useEffect, useState } from "react";
-import postApi from "../apis/postApi";
-import Login from "../views/auth/Login";
-import Bookmark from "../views/bookmark";
-import Register from "../views/auth/Register";
-import Contract from "../views/contract";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '../views/layout';
+import Home from '../views/home';
+import PostDetail from '../views/post-detail';
+import PostRegister from '../views/post-register';
+import RequestViewHouse from '../views/request';
+import Manager from '../views/manager';
+import { createContext } from 'react';
+import postApi from '../apis/postApi';
+import Login from '../views/auth/Login';
+import Bookmark from '../views/bookmark';
+import Register from '../views/auth/Register';
+import Contract from '../views/contract';
+import { Spin } from 'antd';
 
 export const PostContext = createContext();
 
@@ -19,7 +21,7 @@ const AppRoutes = () => {
   const [sortType, setSortType] = useState(0);
   const [curPage, setCurPage] = useState(1);
   const [filterCondition, setFilterCondition] = useState({});
-  const [curNavOption, setCurNavOption] = useState("home");
+  const [curNavOption, setCurNavOption] = useState('home');
   const [useRightFilter, setUseRightFilter] = useState(false);
   const [priceRangeRF, setPriceRangeRF] = useState();
   const [areaRangeRF, setAreaRangeRF] = useState();
@@ -28,16 +30,17 @@ const AppRoutes = () => {
   const [isSearchLoading, setIsSearchLoading] = useState(false);
 
   const getListPost = async () => {
-    try {      
+    setIsSearchLoading(true); // Bắt đầu loading
+    try {
       const res = await postApi.getList({
         sortType: sortType,
         ...filterCondition,
       });
       setListPost(res);
-      // console.log(listPost);
-      setIsSearchLoading(false);
     } catch (e) {
-      setIsSearchLoading(false);
+      console.error('Error fetching posts:', e);
+    } finally {
+      setIsSearchLoading(false); // Kết thúc loading
     }
   };
 
@@ -80,15 +83,21 @@ const AppRoutes = () => {
               }}
             >
               <Layout>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/house/:postId" element={<PostDetail />} />
-                  <Route path="/bookmarks" element={<Bookmark />} />
-                  <Route path="/house_register" element={<PostRegister />} />
-                  <Route path="/house/:houseID/request_view_house/create" element={<RequestViewHouse />} />
-                  <Route path="/:userID/manager" element={<Manager />} />
-                  <Route path="/:userID/contract/create" element={<Contract />} />
-                </Routes>
+                {isSearchLoading ? (
+                  <Spin tip="Loading..." size="large">
+                    <div style={{ minHeight: '300px' }} />
+                  </Spin>
+                ) : (
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/house/:postId" element={<PostDetail />} />
+                    <Route path="/bookmarks" element={<Bookmark />} />
+                    <Route path="/house_register" element={<PostRegister />} />
+                    <Route path="/house/:houseID/request_view_house/create" element={<RequestViewHouse />} />
+                    <Route path="/:userID/manager" element={<Manager />} />
+                    <Route path="/:userID/contract/create" element={<Contract />} />
+                  </Routes>
+                )}
               </Layout>
             </PostContext.Provider>
           }
